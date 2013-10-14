f72c79caf47defb71ee52f82d375354bcd67861b
keywind <jindafeng2009@gmail.com>
2013-10-14 14:36:13 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WeshakeServer::Application.config.secret_key_base = '535f6f1e0435fcf61d0348061a5e04c697f2ea5855c8412818b722edcebdf551e435010e9180b02a25fafd5afb41e71bf57d9efec91f2624da3d4b8bff62e917'
